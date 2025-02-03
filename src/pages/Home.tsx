b98680e3ebb467bcb9bfae426a4cd64e25bfed9e
andrezambroni <andrezambroni@gmail.com>
2025-02-03 15:11:26 -0300

    feat: rotas, nav, error 404

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
-export function Home()
-{
-    return(
-        <div>
-            <h1>Página Home</h1>
-        </div>
-    )
+export function Home() {
+  return (
+    <div>
+      <h1>Página Home</h1>
+
+      <nav>
+        <a href="/products">Produtos</a>
+      </nav>
+    </div>
+  )
 }
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound() {
+  return (
+    <div>
+      <h1>Ops! Essa página nao existe</h1>
+      <a href="/">Voltar</a>
+    </div>
+  )
+}
